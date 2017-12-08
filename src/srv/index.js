95cf55cd9326a8ac2dcc613ff41b23c7d4d30b07
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2017-12-08 15:11:26 -0300

    add POST password

--- src/srv/index.js
+++ src/srv/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const {router} = require('./routes')
 const fs = require('fs');
 const Web3 = require('web3');
+const bodyParser = require('body-parser');
+
 const BookingData = require('../../libs/BookingData.js');
 const HotelManager = require('../../libs/HotelManager.js');
 const HotelEvents = require('../../libs/HotelEvents.js');
@@ -9,22 +11,18 @@ const User = require('../../libs/User.js');
 const Utils = require('../../libs/Utils.js');
 
 const CONFIG = require('../../config.json');
-
-console.log('Config:', CONFIG);
-
 const web3 = new Web3(new Web3.providers.HttpProvider(CONFIG.web3Provider));
 
 const privateKeyString = fs.readFileSync(`${CONFIG.privateKeyDir}`, "utf8");
 const privateKeyJSON = JSON.parse(privateKeyString);
 
-console.log('API ETH address:', privateKeyJSON.address);
-
 const app = express()
-app.use(router)
 
+app.use(bodyParser.json())
+app.use(router)
 app.use((err, req, res, next) => {
   console.error(err)
-  res.status(400).json(err)
+  res.sendStatus(400).json(err)
 })
 
 app.listen(3000, () => {
