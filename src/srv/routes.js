860f7699ad2897f6081977768000d21dac011ab4
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2017-12-13 18:25:17 -0300

    add GET /hotels

--- src/srv/routes.js
+++ src/srv/routes.js
@@ -1,8 +1,17 @@
+const Web3 = require('web3')
 const express = require('express')
 const router = express.Router()
 const CONFIG = require('../../config.json')
 const { loadAccount, updateAccountPassword } = require('../helpers/crypto')
 const { validatePasswords } = require('../helpers/validators')
+
+const BookingData = require('../../libs/BookingData.js')
+const HotelManager = require('../../libs/HotelManager.js')
+const HotelEvents = require('../../libs/HotelEvents.js')
+const User = require('../../libs/User.js')
+const Utils = require('../../libs/Utils.js')
+
+const web3 = new Web3(new Web3.providers.HttpProvider(CONFIG.web3Provider))
 
 router.get('/', (req, res) => {
   res.write('WT Nodejs API')
@@ -17,6 +26,28 @@ router.post('/password', validatePasswords, (req, res, next) => {
     return next({code: 'web3', err})
   }
   res.sendStatus(200)
+})
+
+router.get('/hotels', async (req, res, next) => {
+  // const {password} = req.
+  let ownerAccount = {}
+  try {
+    ownerAccount = web3.eth.accounts.decrypt(loadAccount(CONFIG.privateKeyDir), 'test123')
+  } catch (err) {
+    return next({code: 'web3', err})
+  }
+  try {
+    const hotelManager = new HotelManager({
+      indexAddress: CONFIG.indexAddress,
+      owner: ownerAccount.address,
+      gasMargin: 1.5,
+      web3: web3
+    })
+    const hotels = await hotelManager.getHotels()
+    res.status(200).json(hotels)
+  } catch (err) {
+    return next({code: 'hotelManager', err})
+  }
 })
 
 module.exports = {
