e51bd3a3023fd1b49370207d7fbb72e4a3356dd9
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2017-12-18 14:54:20 -0300

    add unit type to hotel

--- src/srv/routes/unit-types.js
+++ src/srv/routes/unit-types.js
@@ -2,6 +2,9 @@ const Web3 = require('web3')
 const express = require('express')
 const unitTypesRouter = express.Router()
 const CONFIG = require('../../config.json')
+const { loadAccount } = require('../helpers/crypto')
+const { validatePassword } = require('../helpers/validators')
+
 const { handle } = require('../../errors')
 const HotelManager = require('../../libs/HotelManager.js')
 
@@ -19,6 +22,26 @@ unitTypesRouter.get('/hotels/:address/unitTypes', async (req, res, next) => {
     const hotel = await hotelManager.getHotel(address)
     res.status(200).json({
       unitTypes: hotel.unitTypes
+    })
+  } catch (err) {
+    next(handle('web3', err))
+  }
+})
+
+unitTypesRouter.post('/hotels/:address/unitTypes', validatePassword, async (req, res, next) => {
+  const { password, unitType } = req.body
+  const { address } = req.params
+  let ownerAccount = {}
+  try {
+    ownerAccount = web3.eth.accounts.decrypt(loadAccount(CONFIG.privateKeyDir), password)
+    context.owner = ownerAccount.addres
+    const hotelManager = new HotelManager(context)
+    hotelManager.web3.eth.accounts.wallet.add(ownerAccount)
+    const { logs } = await hotelManager.addUnitType(address, unitType)
+    hotelManager.web3.eth.accounts.wallet.remove(ownerAccount)
+    context.owner = undefined
+    res.status(200).json({
+      txHash: logs[0].transactionHash
     })
   } catch (err) {
     next(handle('web3', err))
