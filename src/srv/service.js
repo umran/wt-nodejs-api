a4e4a9e849a17de1e85b64e0bc7439cd28b4b506
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2017-12-22 11:10:50 -0300

    update index for testeability

---
+++ src/srv/service.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const { router } = require('./routes')
+const { unitTypesRouter } = require('./routes/unit-types')
+const { amenitiesRouter } = require('./routes/amenities')
+const bodyParser = require('body-parser')
+const swaggerUi = require('swagger-ui-express')
+const app = express()
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(require('../../docs/swagger.json')))
+app.use(bodyParser.json())
+app.use(router)
+app.use(unitTypesRouter)
+app.use(amenitiesRouter)
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(400).json({
+    code: err.code,
+    short: err.short,
+    long: err.long
+  })
+})
+
+module.exports = {
+  app
+}
