e58ce8edb56031690587ed8f2c30f57c8bb5c241
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2017-12-26 18:58:46 -0300

    hotel images tests

---
+++ tests/http-server/hotel-images.spec.js
@@ -0,0 +1,150 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+const { expect } = require('chai')
+const fetch = require('node-fetch')
+const config = require('../../config.js')
+const { AfterEach,
+        BeforeEach,
+        Before } = require('../hooks.js')
+
+describe('Hotel images', function () {
+  AfterEach()
+  BeforeEach()
+  Before()
+  it('POST /hotels/:address/images. Expect 200', async () => {
+    const body = JSON.stringify({
+      'password': config.get('password'),
+      'url': 'test.jpeg'
+    })
+    await fetch(`http://localhost:3000/hotels/${config.get('testAdress')}/images`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body
+    })
+      .then(async response => {
+        expect(response).to.be.ok
+        const res = await response.json()
+        expect(response).to.have.property('status', 200)
+      })
+  })
+  it('POST /hotels/:address/images. Expect 400 #web3', async () => {
+    const body = JSON.stringify({
+      'password': config.get('password'),
+      'url': 'http://images.com/123'
+    })
+    const address = '0x0000000000000000000000000000000000000000000000000000000000000000'
+    await fetch(`http://localhost:3000/hotels/${address}/images`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body
+    })
+      .then(async response => {
+        expect(response).to.be.ok
+        const res = await response.json()
+        expect(response).to.have.property('status', 400)
+        expect(res).to.have.property('code', '#web3')
+      })
+  })
+  it('POST /hotels/:address/images. Expect 400 #missingPassword', async () => {
+    const body = JSON.stringify({
+      'url': 'http://images.com/123'
+    })
+    const address = '0x0000000000000000000000000000000000000000000000000000000000000000'
+    await fetch(`http://localhost:3000/hotels/${address}/images`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body
+    })
+      .then(async response => {
+        expect(response).to.be.ok
+        const res = await response.json()
+        expect(response).to.have.property('status', 400)
+        expect(res).to.have.property('code', '#missingPassword')
+      })
+  })
+  it('POST /hotels/:address/images. Expect 400 #missingUrl', async () => {
+    const body = JSON.stringify({
+      'password': config.get('password')
+    })
+    const address = '0x0000000000000000000000000000000000000000000000000000000000000000'
+    await fetch(`http://localhost:3000/hotels/${address}/images`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body
+    })
+      .then(async response => {
+        expect(response).to.be.ok
+        const res = await response.json()
+        expect(response).to.have.property('status', 400)
+        expect(res).to.have.property('code', '#missingUrl')
+      })
+  })
+
+  it('DELETE /hotels/:address/images/:id. Expect 200', async () => {
+    const body = JSON.stringify({
+      'password': config.get('password')
+    })
+    await fetch(`http://localhost:3000/hotels/${config.get('testAdress')}/images/0`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body
+    })
+      .then(async response => {
+        expect(response).to.be.ok
+        expect(response).to.have.property('status', 204)
+      })
+  })
+  it('DELETE /hotels/:address/images/:id/0. Expect 400 #web3', async () => {
+    const body = JSON.stringify({
+      'password': config.get('password')
+    })
+    const address = '0x0000000000000000000000000000000000000000000000000000000000000000'
+    await fetch(`http://localhost:3000/hotels/${address}/images/0`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body
+    })
+      .then(async response => {
+        expect(response).to.be.ok
+        const res = await response.json()
+        expect(response).to.have.property('status', 400)
+        expect(res).to.have.property('code', '#web3')
+      })
+  })
+  it('DELETE /hotels/:address/images/:id/0. Expect 400 #missingPassword', async () => {
+    const body = JSON.stringify({})
+    const address = '0x0000000000000000000000000000000000000000000000000000000000000000'
+    await fetch(`http://localhost:3000/hotels/${address}/images/0`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body
+    })
+      .then(async response => {
+        expect(response).to.be.ok
+        const res = await response.json()
+        expect(response).to.have.property('status', 400)
+        expect(res).to.have.property('code', '#missingPassword')
+      })
+  })
+})
