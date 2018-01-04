f97c5c6d5b7ea02fc7a145f6ba8e4296306d83b3
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-01-04 11:58:26 -0300

    Get hotel endpoint

--- src/srv/routes/hotels.js
+++ src/srv/routes/hotels.js
@@ -10,6 +10,29 @@ const { handle } = require('../../../errors')
 const HotelManager = require('../../../libs/HotelManager.js')
 const config = require('../../../config.js')
 
+hotelsRouter.get('/hotels/:address', validatePassword, async (req, res, next) => {
+  const { password } = req.body
+  const { address } = req.params
+  let ownerAccount = {}
+  try {
+    ownerAccount = config.get('web3').eth.accounts.decrypt(loadAccount(config.get('privateKeyDir')), password)
+  } catch (err) {
+    return next(handle('web3', err))
+  }
+  try {
+    const hotelManager = new HotelManager({
+      indexAddress: config.get('indexAddress'),
+      owner: ownerAccount.address,
+      gasMargin: config.get('gasMargin'),
+      web3: config.get('web3')
+    })
+    const hotel = await hotelManager.getHotel(address)
+    res.status(200).json(hotel)
+  } catch (err) {
+    return next(handle('hotelManager', err))
+  }
+})
+
 hotelsRouter.get('/hotels', validatePassword, async (req, res, next) => {
   const { password } = req.body
   let ownerAccount = {}
