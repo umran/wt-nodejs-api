b3d0d61e85005f82b793a8bdf405de58edfeb6f5
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-01-19 09:36:48 -0300

    get hotel bookings endpoint

--- src/srv/routes/hotel-bookings.js
+++ src/srv/routes/hotel-bookings.js
@@ -6,6 +6,7 @@ const { validatePassword,
         validateRequired } = require('../../helpers/validators')
 const { handle } = require('../../../errors')
 const HotelManager = require('../../../wt-js-libs/dist/node/HotelManager.js')
+const BookingData = require('../../../wt-js-libs/dist/node/BookingData.js')
 
 const config = require('../../../config.js')
 
@@ -30,6 +31,18 @@ validatePassword, validateRequired, async(req, res, next) => {
     res.status(200).json({
       txHash: logs[0].transactionHash
     })
+  } catch (err) {
+    next(handle('web3', err))
+  }
+})
+
+hotelBookingRouter.get('/hotels/:hotelAddress/bookings', async (req, res, next) => {
+  const { block } = req.body
+  const { hotelAddress } = req.params
+  try {
+    const data = new BookingData(config.get('web3'))
+    const bookings = await data.getBookings(hotelAddress, block)
+    res.status(200).json({bookings})
   } catch (err) {
     next(handle('web3', err))
   }
