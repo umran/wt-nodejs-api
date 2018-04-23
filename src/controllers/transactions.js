68eeb3acd8cb3fe5d001a6a859d332270a227493
Jirka Chadima <chadima.jiri@gmail.com>
2018-04-23 13:12:10 +0200

    feature: Implements /transactions endpoint and adds tests for it

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,10 +1,13 @@
-const getStatuses = (req, res, next) => {
-  res.status(200).json({
-    meta: {
-      total: 0,
-    },
-    results: {},
-  });
+const wtJsLibs = require('../services/wt-js-libs');
+
+const TX_IDS_SEPARATOR = ',';
+
+const getStatuses = async (req, res, next) => {
+  let txIds = [];
+  if (req.query.transactionIds) {
+    txIds = req.query.transactionIds.split(TX_IDS_SEPARATOR);
+  }
+  res.status(200).json(await wtJsLibs.getInstance().getTransactionsStatus(txIds));
 };
 
 module.exports = {
