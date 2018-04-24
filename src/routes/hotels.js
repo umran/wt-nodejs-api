ce645141f9cb52ff13193749023ad5c529912e13
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-04-24 19:01:31 -0300

    feat(errors): add middleware to load wallet

--- src/routes/hotels.js
+++ src/routes/hotels.js
@@ -4,20 +4,21 @@ const {
   validatePassword,
   // validateHotelInfo,
 } = require('../helpers/validators');
+const { accountLoader } = require('../middlewares/accounts');
 const hotels = require('../controllers/hotels');
 
 const hotelRoute = '/hotels/:hotelAddress';
 const hotelsRoute = '/hotels';
 
-hotelsRouter.post(hotelsRoute, validatePassword, hotels.create);
+hotelsRouter.post(hotelsRoute, validatePassword, accountLoader, hotels.create);
 
 hotelsRouter.get(hotelsRoute, hotels.findAll);
 
 hotelsRouter.get(hotelRoute, hotels.find);
 
-hotelsRouter.put(hotelRoute, validatePassword, hotels.update);
+hotelsRouter.put(hotelRoute, validatePassword, accountLoader, hotels.update);
 
-hotelsRouter.delete(hotelRoute, validatePassword, hotels.remove);
+hotelsRouter.delete(hotelRoute, validatePassword, accountLoader, hotels.remove);
 
 module.exports = {
   hotelsRouter,
