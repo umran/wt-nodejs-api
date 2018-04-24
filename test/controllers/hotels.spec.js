63a2a4d5e36a34c4dd1ce0b5e96de4a3793102f9
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-04-24 19:07:36 -0300

    test(hotels): fix and extend some tests

--- test/controllers/hotels.spec.js
+++ test/controllers/hotels.spec.js
@@ -25,35 +25,62 @@ describe('Hotels', function () {
       .set('content-type', 'application/json')
       .set('accept', 'application/json')
       .expect((res) => {
-        expect(res.body.length).to.be.eql(1);
+        expect(res.body.hotels.length).to.be.eql(1);
       })
       .expect(200, done);
   });
 
   it('GET /hotels/:hotelAddress. Expect 200', (done) => {
-    request(server)
-      .get(`/hotels/${config.get('testAddress')}`)
-      .set('content-type', 'application/json')
-      .set('accept', 'application/json')
-      .expect((res) => {
-        expect(res.body).to.have.property('name', 'Test hotel');
-        expect(res.body).to.have.property('description', 'Test Hotel description');
-      })
-      .expect(200, done);
-  });
+    const name = 'new hotel';
+    const description = 'some description';
+    const manager = '0xd39ca7d186a37bb6bf48ae8abfeb4c687dc8f906';
+    const location = {
+      latitude: 50.0754789,
+      longitude: 14.4225864,
+    };
 
-  it('POST /hotels. Expect 202', (done) => {
-    const hotelName = 'Test Hotel';
-    const hotelDesc = 'Natural and charming atmosphere';
     request(server)
       .post('/hotels')
       .set('content-type', 'application/json')
       .set('accept', 'application/json')
       .set(PASSWORD_HEADER, config.get('password'))
-      .send({
-        'description': hotelDesc,
-        'name': hotelName,
-      })
+      .send({ name, description, manager, location })
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).to.have.property('address');
+        expect(res.body).to.have.property('transactionIds');
+        expect(res.body.transactionIds.length).to.be.above(0);
+        request(server)
+          .get(`/hotels/${res.body.address}`)
+          .set('content-type', 'application/json')
+          .set('accept', 'application/json')
+          .set(PASSWORD_HEADER, config.get('password'))
+          .send({ description: 'Best hotel.', name: 'WTHotel', url: 'new-url' })
+          .expect((res) => {
+            expect(res.body.hotel).to.have.property('name');
+            expect(res.body.hotel).to.have.property('description');
+            expect(res.body.hotel).to.have.property('manager');
+            expect(res.body.hotel).to.have.property('location');
+          })
+          .expect(200, done);
+      });
+  });
+
+  it('POST /hotels. Expect 202', (done) => {
+    const name = 'new hotel';
+    const description = 'some description';
+    const manager = '0xd39ca7d186a37bb6bf48ae8abfeb4c687dc8f906';
+    const location = {
+      latitude: 50.0754789,
+      longitude: 14.4225864,
+    };
+
+    request(server)
+      .post('/hotels')
+      .set('content-type', 'application/json')
+      .set('accept', 'application/json')
+      .set(PASSWORD_HEADER, config.get('password'))
+      .send({ name, description, manager, location })
       .expect((res) => {
         expect(res.body).to.have.property('address');
         expect(res.body).to.have.property('transactionIds');
@@ -69,7 +96,6 @@ describe('Hotels', function () {
       .post('/hotels')
       .set('content-type', 'application/json')
       .set('accept', 'application/json')
-      .set(PASSWORD_HEADER, config.get('password'))
       .send({
         'description': hotelDesc,
         'name': hotelName,
@@ -81,21 +107,36 @@ describe('Hotels', function () {
   });
 
   it('PUT /hotels/:hotelAddress. Expect 202', (done) => {
-    const name = 'WT Hotel';
-    const description = 'Best hotel for developers.';
+    const name = 'new hotel';
+    const description = 'some description';
+    const manager = '0xd39ca7d186a37bb6bf48ae8abfeb4c687dc8f906';
+    const location = {
+      latitude: 50.0754789,
+      longitude: 14.4225864,
+    };
+
     request(server)
-      .put(`/hotels/${config.get('testAddress')}`)
+      .post('/hotels')
       .set('content-type', 'application/json')
       .set('accept', 'application/json')
       .set(PASSWORD_HEADER, config.get('password'))
-      .send({
-        'description': description,
-        'name': name,
-      })
-      .expect((res) => {
-        expect(res.body.length).to.be.above(0);
-      })
-      .expect(202, done);
+      .send({ name, description, manager, location })
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).to.have.property('address');
+        expect(res.body).to.have.property('transactionIds');
+        expect(res.body.transactionIds.length).to.be.above(0);
+        request(server)
+          .put(`/hotels/${res.body.address}`)
+          .set('content-type', 'application/json')
+          .set('accept', 'application/json')
+          .set(PASSWORD_HEADER, config.get('password'))
+          .send({ description: 'Best hotel.', name: 'WTHotel', url: 'new-url' })
+          .expect((res) => {
+            expect(res.body.transactionIds.length).to.be.above(0);
+          })
+          .expect(202, done);
+      });
   });
 
   it('PUT /hotels/:hotelAddress. Expect 401 #missingPassword', (done) => {
@@ -122,7 +163,7 @@ describe('Hotels', function () {
       .set('accept', 'application/json')
       .set(PASSWORD_HEADER, config.get('password'))
       .expect((res) => {
-        expect(res.body.length).to.be.above(0);
+        expect(res.body.transactionIds.length).to.be.above(0);
       })
       .expect(202, done);
   });
