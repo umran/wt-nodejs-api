2840d5cc280ca7538bf2a3946af43b122a992087
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-06-25 10:56:09 +0200

    add field constants

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,30 @@
 const WALLET_PASSWORD_HEADER = 'X-Wallet-Password';
 const WALLET_ID_HEADER = 'X-Wallet-Id';
 
+const DEFAULT_FIELDS = ['id'];
+const DESCRIPTION_FIELDS = [
+  'name',
+  'description',
+  'location',
+  'contacts',
+  'address',
+  'roomTypes',
+  'timezone',
+  'currency',
+  'images',
+  'amenities',
+  'updatedAt',
+];
+
+const HOTEL_FIELDS = [
+  'manager',
+  'id',
+];
+
 module.exports = {
   WALLET_PASSWORD_HEADER,
   WALLET_ID_HEADER,
+  DEFAULT_FIELDS,
+  DESCRIPTION_FIELDS,
+  HOTEL_FIELDS,
 };
